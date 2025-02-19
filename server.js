a6efb74b6c37428dd452e490101cb9e51f913254
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-19 19:05:00 +0530

    - lets goo

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const {Server} = require("socket-io")
+const http = require("http")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection',(socket)=>{
+    console.log("User Connected: ",socket.id)
+
+    socket.on("send all", (data)=>{
+        console.log(data)
+        io.emit("reveive all", data)
+    })
+    
+    socket.on("send personal msg",(data)=>{
+        const {id, message} = data;
+        console.log(data)
+        io.to(id).emit("receive personal msg",({message,id: socket.id}))
+    })
+})
+
+server.listen(4000,()=>{
+    console.log("online")
+})
