b092959155013bd532be3be05f20f1416b9f483f
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-19 19:31:58 +0530

    - transport update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
         origin: '*',
         methods: ['GET', 'POST'],
     },
-    transports: ["websocket","polling"]
+    transports: ["websocket"]
 });
 
 io.on('connection',(socket)=>{
