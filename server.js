401da67b409a2dbc7eeba697a2ec1a34c93f78ac
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-03-11 00:22:31 +0530

    -see

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
@@ -13,25 +14,26 @@ const io = require("socket.io")(server, {
     }
 });
 
-io.on('connection',(socket)=>{
-    console.log("User Connected: ",socket.id)
 
-    socket.on("send all", (data)=>{
-        console.log(data)
-        io.emit("reveive all", data)
-    })
-    
-    socket.on("send personal msg",(data)=>{
-        const {id, message} = data;
-        console.log(data)
-        io.to(id).emit("receive personal msg",({message,id: socket.id}))
-    })
-
-    socket.on("disconnect",(msg)=>{
-        console.log("disconnected: ",msg)
-    })
-})
 
 server.listen(4000,()=>{
     console.log("online")
+    io.on('connection',(socket)=>{
+        console.log("User Connected: ",socket.id)
+    
+        socket.on("send all", (data)=>{
+            console.log(data)
+            io.emit("reveive all", data)
+        })
+        
+        socket.on("send personal msg",(data)=>{
+            const {id, message} = data;
+            console.log(data)
+            io.to(id).emit("receive personal msg",({message,id: socket.id}))
+        })
+    
+        socket.on("disconnect",(msg)=>{
+            console.log("disconnected: ",msg)
+        })
+    })
 })
